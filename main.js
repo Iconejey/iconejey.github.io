65ccdbff967a475577aa84271d2b30235795af18
Iconejey <43481095+Iconejey@users.noreply.github.com>
2020-08-05 17:15:22 -0400

    05/08/2020

--- main.js
+++ main.js
@@ -2,9 +2,30 @@ const code = '######## ##################### ############ ########### ###### #\n
 	'\n'
 );
 
+window.isUpdateAvailable = new Promise(function (resolve, reject) {
+	if ('serviceWorker' in navigator)
+		navigator.serviceWorker
+			.register('/sw.js', {
+				scope: '/'
+			})
+			.then(reg => {
+				reg.onupdatefound = () => {
+					const installingWorker = reg.installing;
+					installingWorker.onstatechange = () => {
+						if (installingWorker.state == 'installed') {
+							if (navigator.serviceWorker.controller) resolve(true);
+							else resolve(false);
+						}
+					};
+				};
+			});
+}).then(isAvailable => {
+	if (isAvailable && confirm("Mise à jour disponible ! Recharger l'application?")) location.reload();
+});
+
 let codeElem = document.getElementById('svg');
 let pjob = document.getElementById('job');
-let line = document.getElementById('line');
+let lines = document.getElementsByClassName('line');
 
 const setSvg = (start, end, offset) => {
 	let svgCode = '';
@@ -21,20 +42,13 @@ const setSvg = (start, end, offset) => {
 			if (!' \t\n'.includes(c)) {
 				if (!trait) {
 					trait = offx;
-					svgCode += `<circle cx='${Math.floor(offx * sc)}' cy='${Math.floor(
-						offy * sc
-					)}' r='${r}' fill='${color}' />`;
+					svgCode += `<circle cx='${Math.floor(offx * sc)}' cy='${Math.floor(offy * sc)}' r='${r}' fill='${color}' />`;
 				}
 			} else {
 				if (trait) {
-					svgCode += `<circle cx='${Math.floor((offx - 1) * sc)}' cy='${Math.floor(
-						offy * sc
-					)}' r='${r}' fill='${color}' />`;
+					svgCode += `<circle cx='${Math.floor((offx - 1) * sc)}' cy='${Math.floor(offy * sc)}' r='${r}' fill='${color}' />`;
 
-					if (trait - offx - 1)
-						svgCode += `<line x1='${trait * sc}' y1='${Math.floor(offy * sc)}' x2='${Math.floor(
-							(offx - 1) * sc
-						)}' y2='${Math.floor(offy * sc)}' style='stroke: ${color}; stroke-width: ${r * 2};' />`;
+					if (trait - offx - 1) svgCode += `<line x1='${trait * sc}' y1='${Math.floor(offy * sc)}' x2='${Math.floor((offx - 1) * sc)}' y2='${Math.floor(offy * sc)}' style='stroke: ${color}; stroke-width: ${r * 2};' />`;
 
 					trait = null;
 				}
@@ -51,44 +65,11 @@ const setSvg = (start, end, offset) => {
 let frame = 0;
 const tick = () => {
 	frame++;
-	setSvg(Math.floor(frame / 20), Math.floor(frame / 20 + 24), (frame % 20) / 10);
+	setSvg(Math.floor(frame / 20), Math.floor(frame / 20 + 26), (frame % 20) / 10);
 	pjob.innerHTML = 'Étudiant L2 Info';
 	if ((frame / 30) % 2 > 1) pjob.innerHTML += '_';
-	line.innerHTML = `<line x1='${window.innerWidth / 10}' y1='0' x2='${
-		(window.innerWidth / 10) * 9
-	}' y2='0' style='stroke: #eee; stroke-width: 2;' />`;
+	for (let line of lines) line.innerHTML = `<line x1='${window.innerWidth / 10}' y1='0' x2='${(window.innerWidth / 10) * 9}' y2='0' style='stroke: #eee; stroke-width: 2;' />`;
 	requestAnimationFrame(tick);
 };
 
-window.onload = () => {
-	tick();
-	new Glider(document.querySelector('.glider'), {
-		slidesToShow: 1,
-		dots: '#dots',
-		draggable: true,
-		arrows: {
-			prev: '.glider-prev',
-			next: '.glider-next'
-		},
-		responsive: [
-			{
-				breakpoint: 1100,
-				settings: {
-					slidesToShow: 2.5
-				}
-			},
-			{
-				breakpoint: 900,
-				settings: {
-					slidesToShow: 2
-				}
-			},
-			{
-				breakpoint: 600,
-				settings: {
-					slidesToShow: 1.5
-				}
-			}
-		]
-	});
-};
+window.onload = () => tick();
