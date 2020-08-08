6fc91983ae06d697b7c68b00c63901553e10faff
Iconejey <43481095+Iconejey@users.noreply.github.com>
2020-08-07 23:13:47 -0400

    07/08/2020

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-const code = '######## ##################### ############ ########### ###### #\n	######################################## ##### ## #\n		#######################\n		### #### # # ## # # ############################ #### #\n			### # # ########################\n\n			### # # ######### # ######\n				# # ######### # ######\n			######################### # ### ######### ## # # ############### # # # ### # #### #######\n		#\n	###\n\n	####################################### ##### ## #\n		#######################\n		### #### # # ## # # ############################ #### #\n			### # # ########################\n			### # # ##########################\n\n			## ### #\n				################## # ###### ######### # #######\n			#\n		#\n	###\n\n	###################################### ##### ## #\n		#######################\n		### #### # # ## # # ############################ #### #\n			### # # ########################\n			### # # ##########################\n\n			## ### #\n				################# ##### ########### # ## # ##### # ########### ####### # ###\n				################################ ###\n			#\n		#\n		################## # #####################\n	###\n#\n\n##### ###### #\n	############## ## ##### ###### #\n		########## # # ## ## ## # ##\n		######## # # ## ## ## # ##\n		######### # #####\n		######### # # ### ## # ###### #### ## # ##### ##\n	#\n\n	######## #\n		###### # ## ########## # ############# ## ########## # ############ ##\n	#\n\n	######### #\n		### # # ##############\n		###### ############# # ### # ### # #####\n	#\n\n	######### ## #\n		########## # ##\n		########## # ##\n\n		### # # ##############\n		### # # ###############\n\n		## ## # ############## #\n			############ # ########## # #### # ## # ##############\n			############ # ########## # #### # ## # ##############\n		#\n	#\n\n	######### ###### #\n		############# # ######\n		############### # ######\n		############# # ##\n\n		################\n		##################### ############# ############## ## # # #########\n		#############\n\n		################\n		################### ########### ############# ## # # #########\n		###########\n	#\n#\n\n##### ###### #\n	############## ## ########### ############## #\n		######### # ###########\n		######### # ##############\n		######## # ##########\n\n		###### # ##\n		###### # ##\n	#\n\n	########## ## ###### ##### #\n		# ## ######\n		# ## ######\n		## ####### # # ## # # ###### # ############## ## ###### # # ## # # ###### # ################ #\n			######## # ##########\n			#######\n		#\n	#\n\n	############ #\n		####################### ####### ########\n	#\n#'.split(
-	'\n'
-);
-
 window.isUpdateAvailable = new Promise(function (resolve, reject) {
 	if ('serviceWorker' in navigator)
 		navigator.serviceWorker
@@ -23,53 +19,37 @@ window.isUpdateAvailable = new Promise(function (resolve, reject) {
 	if (isAvailable && confirm("Mise à jour disponible ! Recharger l'application?")) location.reload();
 });
 
-let codeElem = document.getElementById('svg');
-let pjob = document.getElementById('job');
-let lines = document.getElementsByClassName('line');
-
-const setSvg = (start, end, offset) => {
-	let svgCode = '';
-	let sc = 6;
-	let r = 3;
-	let color = '#35363A';
-
-	let offy = -offset;
-	for (let i = start; i < end; i++) {
-		let line = code[i % code.length];
-		let offx = 2;
-		let trait = null;
-		for (let c of line) {
-			if (!' \t\n'.includes(c)) {
-				if (!trait) {
-					trait = offx;
-					svgCode += `<circle cx='${Math.floor(offx * sc)}' cy='${Math.floor(offy * sc)}' r='${r}' fill='${color}' />`;
-				}
-			} else {
-				if (trait) {
-					svgCode += `<circle cx='${Math.floor((offx - 1) * sc)}' cy='${Math.floor(offy * sc)}' r='${r}' fill='${color}' />`;
-
-					if (trait - offx - 1) svgCode += `<line x1='${trait * sc}' y1='${Math.floor(offy * sc)}' x2='${Math.floor((offx - 1) * sc)}' y2='${Math.floor(offy * sc)}' style='stroke: ${color}; stroke-width: ${r * 2};' />`;
-
-					trait = null;
-				}
-			}
-
-			offx += c == '\t' ? 4 : 1;
-		}
-		offy += 2;
+const setPage = (page_id, val) => {
+	page = document.getElementById(page_id);
+	let main = document.getElementById('main');
+	if (val) {
+		setTimeout(() => {
+			main.classList.add('no-display');
+		}, 500);
+		main.classList.add('hide');
+		page.classList.add('slidein');
+		location.hash = page_id + '-page';
+	} else {
+		main.classList.remove('no-display');
+		main.classList.remove('hide');
+		page.classList.remove('slidein');
+		location.hash = '#home';
 	}
-
-	codeElem.innerHTML = svgCode;
 };
 
-let frame = 0;
-const tick = () => {
-	frame++;
-	setSvg(Math.floor(frame / 20), Math.floor(frame / 20 + 26), (frame % 20) / 10);
-	pjob.innerHTML = 'Étudiant L2 Info';
-	if ((frame / 30) % 2 > 1) pjob.innerHTML += '_';
-	for (let line of lines) line.innerHTML = `<line x1='${window.innerWidth / 10}' y1='0' x2='${(window.innerWidth / 10) * 9}' y2='0' style='stroke: #eee; stroke-width: 2;' />`;
-	requestAnimationFrame(tick);
+const movePage = (from, to) => {
+	setPage(from, false);
+	setTimeout(() => {
+		setPage(to, true);
+	}, 600);
 };
 
-window.onload = () => tick();
+onload = () => {
+	if (location.hash && location.hash != '#home') setPage(location.hash.replace('-page', '').replace('#', ''), true);
+
+	let cursor = false;
+	setInterval(() => {
+		cursor = !cursor;
+		document.getElementById('job').innerHTML = cursor ? 'Étudiant L2 Info_' : 'Étudiant L2 Info';
+	}, 500);
+};
